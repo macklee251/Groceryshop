08b836c7c85493c99639659a501ab20e4c5927b4
mack lee <114943402+macklee251@users.noreply.github.com>
2024-03-08 17:56:46 -0300

    .

--- model.py
+++ model.py
@@ -1,8 +1,29 @@
-from datetime import datetime, date
 class Categoria:
-    def __init__(self, name:str, description:str) -> None:
-        self.name = name 
-        self.description = description
+    def __init__(self, nome:str, descricao:str=None) -> None:
+        self.nome = nome 
+        self.descricao = descricao
         
     def __str__(self) -> str:
-        return f'name: {self.name} - description: {self.description}'
+        return f'Nome: {self.nome} - Descrição: {self.descricao}'
+    
+class Produto:
+    def __init__(self, nome, categoria:Categoria, preco):
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        
+    def __str__(self) -> str:
+        return f'Nome: {self.nome} - Categoria: {self.categoria} - Preço: {self.preco}'
+    
+class Estoque:
+    def __init__(self, produto:Produto, quantidade:int) -> None:
+        self.produto = produto
+        self.quantidade = quantidade
+        
+    def __str__(self) -> str:
+        return f'Produto: {self.produto} - Quantidade: {self.quantidade}'
+    
+    
+sabonete = Estoque(Produto('Sabonete', Categoria('Higiene', 'produto de higiene pessoal'), 2.50), 100)
+
+print(sabonete)
